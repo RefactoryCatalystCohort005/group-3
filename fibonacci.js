0122315a502dd77b3b90f209d78658f38215950f
Aturinda Ignatius <ignatius.aturinda@gmail.com>
2020-10-07 08:48:53 +0300

    making the loop method for fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,21 +1,25 @@
 
-// function fibonacci(){
-//     var a = 1;
-//     var b = 0;
-//     var sum = 0;
-//     for(let i=1; i<=10; i++){
-//     c=a+b;
-//     sum+=c
-//     console.log(c );
-    
-//     a=b;
-//     b=c;
-    
-//     }
-//     return sum;
-// }
+function fibonacci(d){
+    var a = 1;
+    var b = 0;
+    var sum = 0;
+    for(let i = 0; i <= d; i++){
+        if(i === 0 ){
+            c = 0;
+            console.log(c);
+        }
+        else{
+            c = a + b;
+            sum += c
+            console.log( c );
+            a = b;
+            b = c;
+        }
+    }
+    return sum;
+}
 
-// fibonacci();
+fibonacci(8);
 
 
 function fibonacci1(n){
@@ -26,7 +30,7 @@ function fibonacci1(n){
       var s = fibonacci1(n - 1);
       s.push(s[s.length - 1] + s[s.length - 2]);
       let M = s.indexOf(13);
-      console.log(M)
+      
       return s;
     }
   };
@@ -34,5 +38,5 @@ function fibonacci1(n){
    console.log(fibonacci1(8));
 
 
-// module.exports = fibonacci;
+module.exports = fibonacci;
 module.exports = fibonacci1;
